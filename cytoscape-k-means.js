922f288b02857fdfd68c045816aa98faf7f29ff0
Zoe Xi <email@zoexi.com>
2016-05-29 06:15:05 -0700

    cleanup for demo

--- cytoscape-k-means.js
+++ cytoscape-k-means.js
@@ -8,77 +8,75 @@
     maxIterations: 10,
     attributes: [
       function(node) {
-        return node.position('x')
+        return node.position('x');
       },
       function(node) {
-        return node.position('y')
+        return node.position('y');
       }
     ]
   };
 
   var setOptions = function( opts, options ) {
-    for (var i in options) { opts[i] = defaults[i]; }
-    for (var i in options) { opts[i] = options[i]; }
+    for (var i in defaults) { opts[i] = defaults[i]; }
+    for (var i in options)  { opts[i] = options[i];  }
   };
 
   var distances = {
-    euclidean: function (node, centroid, attributes) {
+    euclidean: function ( node, centroid, attributes ) {
       var total = 0;
-      for (var dim = 0; dim < attributes.length; dim++) {
-        total += Math.pow(attributes[dim](node) - centroid[dim], 2);
+      for ( var dim = 0; dim < attributes.length; dim++ ) {
+        total += Math.pow( attributes[dim](node) - centroid[dim], 2 );
       }
       return Math.sqrt(total);
     },
-    manhattan: function (node, centroid, attributes) {
+    manhattan: function ( node, centroid, attributes ) {
       var total = 0;
-      for (var dim = 0; dim < attributes.length; dim++) {
-        total += Math.abs(attributes[dim](node) - centroid[dim]);
+      for ( var dim = 0; dim < attributes.length; dim++ ) {
+        total += Math.abs( attributes[dim](node) - centroid[dim] );
       }
       return total;
     },
-    max: function (node, centroid, attributes) {
+    max: function ( node, centroid, attributes ) {
       var max = 0;
-      for (var dim = 0; dim < attributes.length; dim++) {
-        max = Math.max(max, Math.abs(attributes[dim](node) - centroid[dim]));
+      for ( var dim = 0; dim < attributes.length; dim++ ) {
+        max = Math.max( max, Math.abs( attributes[dim](node) - centroid[dim] ) );
       }
       return max;
     }
   };
 
   var randomCentroids = function( nodes, k, attributes ) {
-    var dim = attributes.length,
-        min = new Array(dim),
-        max = new Array(dim),
-        centroids = new Array(k);
+    var ndim = attributes.length,
+        min  = new Array(ndim),
+        max  = new Array(ndim),
+        centroids = new Array(k),
+        centroid  = null;
 
-    // Find min, max values across each attribute dimension
-    for (var i = 0; i < dim; i++) {
+    // Find min, max values for each attribute dimension
+    for ( var i = 0; i < ndim; i++ ) {
       min[i] = nodes.min( attributes[i] ).value;
       max[i] = nodes.max( attributes[i] ).value;
     }
 
     // Build k centroids, each represented as an n-dim feature vector
-    for (var c = 0; c < k; c++) {
-      var centroid = [];
-      for (i = 0; i < dim; i++) {
+    for ( var c = 0; c < k; c++ ) {
+      centroid = [];
+      for ( i = 0; i < ndim; i++ ) {
         centroid[i] = Math.random() * (max[i] - min[i]) + min[i]; // random initial value
       }
       centroids[c] = centroid;
     }
 
-    //return centroids;
-
-    // TODO: remove in final version
-    return [ [1.0,1.0], [5.0,7.0] ];
+    return centroids;
   };
 
-  var classify = function(node, centroids, distance, attributes) {
-    var min = Infinity,
+  var classify = function( node, centroids, distance, attributes ) {
+    var min   = Infinity,
         index = 0;
 
     distance = distances[distance];
 
-    for (var i = 0; i < centroids.length; i++) {
+    for ( var i = 0; i < centroids.length; i++ ) {
       var dist = distance(node, centroids[i], attributes);
       if (dist < min) {
         min = dist;
@@ -89,9 +87,9 @@
     return index;
   };
 
-  var hasConverged = function(v1, v2) {
-    v1 = Math.round(v1 * 10000) / 10000;
-    v2 = Math.round(v2 * 10000) / 10000;
+  var hasConverged = function( v1, v2, roundFactor ) {
+    v1 = Math.round( v1 * Math.pow(10, roundFactor) ) / Math.pow(10, roundFactor); // truncate to 'roundFactor' decimal places
+    v2 = Math.round( v2 * Math.pow(10, roundFactor) ) / Math.pow(10, roundFactor);
     return v1 === v2;
   };
 
@@ -101,77 +99,68 @@
     if( !cytoscape ){ return; } // can't register if cytoscape unspecified
     
     cytoscape( 'collection', 'kMeans', function( options ){
-      var eles = this;
-      var cy = this.cy();
-      var nodes = this.nodes();                         // => nodes[0].position() => { x: 0, y: 0 }
-      var node = null;
-      var opts = {};
+      var cy    = this.cy(),
+          nodes = this.nodes(),
+          node  = null,
+          opts  = {};
 
       // Set parameters of algorithm: # of clusters, distance metric, etc.
       setOptions( opts, options );
 
       // Begin k-means algorithm
+      var clusters   = new Array(opts.k),
+          assignment = {};
 
       // Step 1: Initialize centroid positions
-      var centroids = randomCentroids(nodes, opts.k, opts.attributes);
-      var assignment = {};
-      var clusters = new Array(opts.k);
+      var centroids  = randomCentroids( nodes, opts.k, opts.attributes ),
+          isStillMoving   = true,
+          iterations = 0;
 
-      var iterations = 0;
-      var movement = true;
-      while ( movement && iterations < opts.maxIterations ) {
-        movement = false;
+      while ( isStillMoving && iterations < opts.maxIterations ) {
 
-        // Step 2: Assign nodes to the nearest centroid.
+        // Step 2: Assign nodes to the nearest centroid
         for ( var n = 0; n < nodes.length; n++ ) {
           node = nodes[n];
-          // determine which cluster this node belongs to
-          assignment[ node.id() ] = classify(node, centroids, opts.distance, opts.attributes); // node id => centroid #
+          // Determine which cluster this node belongs to: node id => cluster #
+          assignment[ node.id() ] = classify( node, centroids, opts.distance, opts.attributes );
         }
 
-        // Step 3: For each of the k clusters, recalculate its centroid.
-        movement = false;
-        for ( var c = 0; c < opts.k; c++ ) { // for each cluster
+        // Step 3: For each of the k clusters, update its centroid
+        isStillMoving = false;
+        for ( var c = 0; c < opts.k; c++ ) {
           var cluster = cy.collection();
 
-          // find all nodes that belong to this cluster
+          // Get all nodes that belong to this cluster
           for ( n = 0; n < nodes.length; n++ ) {
             node = nodes[n];
-            if (assignment[ node.id() ] === c) {
-              console.log("Node " + node.id() + " belongs in cluster " + c);
+            if ( assignment[ node.id() ] === c ) {
+              //console.log("Node " + node.id() + " belongs in cluster " + c);
               cluster = cluster.union( node );
             }
           }
 
-          if (cluster.empty()) { // if cluster is empty, break out early & move to next cluster
+          if ( cluster.empty() ) { // If cluster is empty, break out early & move to next cluster
             continue;
           }
 
-          // Recalculate centroid position by taking avg attribute of nodes within cluster
-          // and set flag if algorithm has converged/when centroids no longer change.
-          var dim = opts.attributes.length,
-              sum = new Array(dim),
-              centroid = centroids[c], // [ dim_1, dim_2, dim_3, ... , dim_n ]
-              newCentroid = new Array(dim);
+          // Update centroids by calculating avg of all nodes within the cluster.
+          var ndim        = opts.attributes.length,
+              centroid    = centroids[c],           // [ dim_1, dim_2, dim_3, ... , dim_n ]
+              newCentroid = new Array(ndim),
+              sum         = new Array(ndim);
 
-          // for each dim
-          for (var d = 0; d < dim; d++) {
+          for ( var d = 0; d < ndim; d++ ) {
             sum[d] = 0.0;
-            // get avg of attrib from each node
             for ( var i = 0; i < cluster.length; i++ ) {
               node = cluster[i];
               sum[d] += opts.attributes[d](node);
             }
-          }
-
-          for (d = 0; d < dim; d++) {
             newCentroid[d] = sum[d] / cluster.length;
-          }
 
-          // Check to see if algorithm has converged across all dim, i.e. when centroid positions no longer change
-          for (d = 0; d < dim; d++) {
-            if (!hasConverged(newCentroid[d], centroid[d])) // has not converged across this dimension
-                movement = true;
+            // Check to see if algorithm has converged, i.e. when centroids no longer change
+            if ( !hasConverged(newCentroid[d], centroid[d], 4) ) { // approximates to 4 decimal places
+              isStillMoving = true;
+            }
           }
 
           centroids[c] = newCentroid;
@@ -180,7 +169,7 @@
 
         iterations++;
       }
-
+      
       return clusters;
     } );
 
