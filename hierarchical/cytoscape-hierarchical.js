1f63aa054db284925fdf2630cd8a31656211e590
Zoe Xi <email@zoexi.com>
2016-06-12 11:27:34 -0700

    finish hierarchical alg; need to polish & check

--- hierarchical/cytoscape-hierarchical.js
+++ hierarchical/cytoscape-hierarchical.js
@@ -5,7 +5,7 @@
   var defaults = {
     distance: 'euclidean',
     linkage: 'single',
-    threshold: '10',
+    threshold: 10,
     attributes: [
       function(node) {
         return node.position('x');
@@ -46,7 +46,86 @@
   };
 
   var mergeClosest = function( clusters, index, dists, mins, opts ) {
+    // find two closest clusters from cached mins
+    var minKey = 0,
+        min = Infinity;
+    for (var i = 0; i < clusters.length; i++) {
+      var key = clusters[i].key,
+          dist = dists[key][mins[key]];
+      if (dist < min) {
+        minKey = key;
+        min = dist;
+      }
+    }
+    if (min >= opts.threshold) {
+      return false;
+    }
 
+    var c1 = index[minKey],
+        c2 = index[mins[minKey]];
+
+    // merge two closest clusters
+    var merged = {
+      value: c1.value.concat(c2.value),
+      key: c1.key,
+      size: c1.size + c2.size
+    };
+
+    clusters[c1.index] = merged;
+    clusters.splice(c2.index, 1);
+    index[c1.key] = merged;
+
+    // update distances with new merged cluster
+    for (var i = 0; i < clusters.length; i++) {
+      var ci = clusters[i];
+      var dist;
+      if (c1.key == ci.key) {
+        dist = Infinity;
+      }
+      else if (opts.linkage == "single") {
+        dist = dists[c1.key][ci.key];
+        if (dists[c1.key][ci.key] > dists[c2.key][ci.key]) {
+          dist = dists[c2.key][ci.key];
+        }
+      }
+      else if (opts.linkage == "complete") {
+        dist = dists[c1.key][ci.key];
+        if (dists[c1.key][ci.key] < dists[c2.key][ci.key]) {
+          dist = dists[c2.key][ci.key];
+        }
+      }
+      else if (opts.linkage == "average") {
+        dist = (dists[c1.key][ci.key] * c1.size
+            + dists[c2.key][ci.key] * c2.size) / (c1.size + c2.size);
+      }
+      else {
+        dist = distances[opts.distance]( ci.value[0], c1.value[0], opts.attributes );
+      }
+
+      dists[c1.key][ci.key] = dists[ci.key][c1.key] = dist;
+    }
+
+    // update cached mins
+    for (var i = 0; i < clusters.length; i++) {
+      var key1 = clusters[i].key;
+      if (mins[key1] == c1.key || mins[key1] == c2.key) {
+        var min = key1;
+        for (var j = 0; j < clusters.length; j++) {
+          var key2 = clusters[j].key;
+          if (dists[key1][key2] < dists[key1][min]) {
+            min = key2;
+          }
+        }
+        mins[key1] = min;
+      }
+      clusters[i].index = i;
+    }
+
+    // clean up metadata used for clustering
+    delete c1.key; delete c2.key;
+    delete c1.index; delete c2.index;
+
+    return true;
   };
 
   var hierarchical = function( options ){
@@ -59,7 +138,7 @@
     setOptions( opts, options );
 
     // Begin hierarchical algorithm
-    var clusters = new Array(opts.k);
+    var clusters = new Array(nodes.length);
     var dists    = [];
     var mins     = [];
     var index    = [];
@@ -67,7 +146,7 @@
     // In agglomerative (bottom-up) version, each node starts as its own cluster.
     for (var n = 0; n < nodes.length; n++) {
       var cluster = {
-        value: nodes[n],
+        value: [ nodes[n] ],
         key:   n,
         index: n,
         size:  1
@@ -81,7 +160,7 @@
     // Initiate distance matrix
     for (var i = 0; i < clusters.length; i++) {
       for (var j = 0; j <= i; j++) {
-        var dist = (i == j) ? Infinity : distances[opts.distance]( clusters[i].value, clusters[j].value, opts.attributes );
+        var dist = (i == j) ? Infinity : distances[opts.distance]( clusters[i].value[0], clusters[j].value[0], opts.attributes );
         dists[i][j] = dist;
         dists[j][i] = dist;
 
@@ -95,14 +174,17 @@
     while ( merged ) {
       merged = mergeClosest( clusters, index, dists, mins, opts );
     }
-    
-    clusters.forEach( function( cluster ) {
+
+    var retClusters = new Array(clusters.length);
+    clusters.forEach( function( cluster, i ) {
       // Clean up meta data used for clustering
       delete cluster.key;
       delete cluster.index;
+
+      retClusters[i] = cy.collection( cluster.value );
     });
 
-    return clusters;
+    return retClusters;
   };
 
   // registers the extension on a cytoscape lib ref
